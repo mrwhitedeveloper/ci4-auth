e9462bce3ce6b2782caaebd74d708b03aa491525
white developer <mrwhitedeveloper@gmail.com>
2023-05-22 20:48:17 +0530

    title update

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,7 +7,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> <?php echo env('web_app_name'); ?> </title>
+    <title> <?php echo !empty($title)?$title:env('web_app_name'); ?></title>
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
