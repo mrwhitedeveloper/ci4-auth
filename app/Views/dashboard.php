1950e2479b93941790e6b7859ed513544d920f0b
white developer <mrwhitedeveloper@gmail.com>
2023-05-22 20:46:28 +0530

    dashboard form

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,1 +1,20 @@
-<h1>Dashboard</h1>
+<?=$this->extend('layout/main')?>
+
+<?=$this->section('content')?>
+
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <h1>Dashboard</h1>
+        </div>
+       
+    </div>
+</section>
+
+<section class="content">
+<script>
+$(document).ready(function() {
+  
+});
+</script>
+<?=$this->endSection();?>
