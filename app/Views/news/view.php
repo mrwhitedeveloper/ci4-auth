283c8144c8ab6cfce760742a640a156dda16b28c
white developer <mrwhitedeveloper@gmail.com>
2023-05-22 20:47:59 +0530

    news view

--- app/Views/news/view.php
+++ app/Views/news/view.php
@@ -1,2 +1,24 @@
-<h2><?= esc($news['title']) ?></h2>
-<p><?= esc($news['body']) ?></p>
+<div class="container">
+	<div class="card mt-2">
+		<div class="card-header">
+			<div class="user-block">
+				<span class="username">
+					<a href="#"><?= esc($news['title']) ?>
+					</a>
+				</span>
+			</div>
+			<!-- /.user-block -->
+			<div class="card-tools">
+               
+                </div>
+			<!-- /.card-tools -->
+		</div>
+		<!-- /.card-header -->
+		<div class="card-body">
+			<!-- post text -->
+			<p><?= esc($news['body']) ?>
+			</p>
+		</div>
+		<!-- /.card-body -->
+	</div>
+</div>
