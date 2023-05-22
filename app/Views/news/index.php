79c8a9bc84fe52beaee7c12812d1fe45728c6202
white developer <mrwhitedeveloper@gmail.com>
2023-05-22 20:47:35 +0530

    news list

--- app/Views/news/index.php
+++ app/Views/news/index.php
@@ -1,24 +1,31 @@
 <div class="container">
-<h2><?= esc($title) ?></h2>
+	<h2><?= esc($title) ?>
+	</h2>
 
 <?php if (! empty($news) && is_array($news)): ?>
 
     <?php foreach ($news as $news_item): ?>
-
-        <h3><?= esc($news_item['title']) ?></h3>
-
-        <div class="main">
+	<div class="card card-widget">
+		<div class="card-header">
+			<h3><?= esc($news_item['title']) ?>
+			</h3>
+		</div>
+		<div class="card-body">
+			<div class="main">
             <?= esc($news_item['body']) ?>
-        </div>
-        <p><a href="/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
-
+			</div>
+		</div>
+		<div class="card-footer">
+			<p>
+				<a href="/news/<?= esc($news_item['slug'], 'url') ?>">View article</a>
+			</p>
+		</div>
+	</div>
     <?php endforeach ?>
 
 <?php else: ?>
-
-    <h3>No News</h3>
-
-    <p>Unable to find any news for you.</p>
+	<h3>No News</h3>
+	<p>Unable to find any news for you.</p>
 
 <?php endif ?>
 </div>
